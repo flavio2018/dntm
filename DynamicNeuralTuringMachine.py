22a295f785b7448c9ef0c56458b98eef640617e8
flavio2018 <flavio.petruzzellis@gmail.com>
2022-07-17 18:49:54 +0200

    Fix mem reading cut

--- DynamicNeuralTuringMachine.py
+++ DynamicNeuralTuringMachine.py
@@ -52,7 +52,7 @@ class DynamicNeuralTuringMachine(nn.Module):
         self.memory.update(self.controller_hidden_state, x)
         self.controller_hidden_state = self.controller(x, self.controller_hidden_state, self.memory_reading)
         address_size = self.memory.memory_addresses.shape[1] 
-        self.memory_reading = self.memory_reading[:, address_size:]
+        self.memory_reading = self.memory_reading[address_size:, :]
         output = F.log_softmax(self.W_output @ self.memory_reading + self.b_output, dim=0)
         return self.controller_hidden_state, output
 
