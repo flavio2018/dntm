9d8c20423721b58bb4cdd3fac20b562e21025f00
Flavio <flavio.petruzzellis@gmail.com>
2022-10-06 11:30:48 +0200

    Pass vanilla mem reading in output

--- DynamicNeuralTuringMachine.py
+++ DynamicNeuralTuringMachine.py
@@ -54,9 +54,7 @@ class DynamicNeuralTuringMachine(nn.Module):
         # self.memory_reading.register_hook(print)
         self.memory.update(self.controller_hidden_state, x)
         self.controller_hidden_state = self.controller(x, self.controller_hidden_state, self.memory_reading)
-        readings_norm = torch.linalg.norm(self.memory_reading, dim=0)
-        self.normalized_memory_reading = self.memory_reading / readings_norm
-        output = F.log_softmax(self.W_output @ self.normalized_memory_reading + self.b_output, dim=0)
+        output = F.log_softmax(self.W_output @ self.memory_reading + self.b_output, dim=0)
         return self.controller_hidden_state, output
 
     def _init_parameters(self, init_function):
