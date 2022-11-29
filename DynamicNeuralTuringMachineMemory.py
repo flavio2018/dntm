c7af61860e1b454608d880c4506159f483f6490c
Flavio <flavio.petruzzellis@gmail.com>
2022-11-29 16:07:49 +0100

    Duplicate addressing params (MemoryHead)

--- DynamicNeuralTuringMachineMemory.py
+++ DynamicNeuralTuringMachineMemory.py
@@ -4,6 +4,45 @@ import torch.nn.functional as F
 import logging
 
 
+class MemoryHead(nn.Module):
+    def __init__(self, n_locations, location_size, batch_size, hidden_size):
+        super(MemoryHead, self).__init__()
+
+        self.register_buffer("exp_mov_avg_similarity", torch.zeros(size=(n_locations, batch_size)))
+        self.W_hat_hidden = nn.Parameter(torch.zeros(size=(n_locations, hidden_size)))
+
+        # query vector MLP parameters (W_k, b_k)
+        self.W_query = nn.Parameter(torch.zeros(size=(n_locations, location_size)), requires_grad=True)
+        self.b_query = nn.Parameter(torch.zeros(size=(location_size, 1)), requires_grad=True)
+
+        # sharpening parameters (u_beta, b_beta)
+        self.u_sharpen = nn.Parameter(torch.zeros(size=(hidden_size, 1)), requires_grad=True)
+        self.b_sharpen = nn.Parameter(torch.zeros(1), requires_grad=True)
+
+        # LRU parameters (u_gamma, b_gamma)
+        self.b_lru = nn.Parameter(torch.zeros(1))
+        self.u_lru = nn.Parameter(torch.zeros(size=(hidden_size, 1)))
+
+    def address(self, h, M):
+        self.projected_hidden_state = self.W_hat_hidden @ h
+        self.query = self.W_query.T @ self.projected_hidden_state + self.b_query
+        self.sharpening_beta = F.softplus(self.u_sharpen.T @ h + self.b_sharpen) + 1
+        
+        # compute similarity
+        self.similarity_vector = self.sharpening_beta * F.cosine_similarity(M[:, None, :], self.query.T[None, :, :],
+                                                                            eps=1e-7, dim=-1)
+        # lru mechanism
+        self.lru_gamma = torch.sigmoid(self.u_lru.T @ h + self.b_lru)
+        self.lru_similarity_vector = F.softmax(self.similarity_vector - self.lru_gamma * self.exp_mov_avg_similarity, dim=0)
+        with torch.no_grad():
+            self.exp_mov_avg_similarity = 0.1 * self.exp_mov_avg_similarity + 0.9 * self.similarity_vector
+        return self.lru_similarity_vector
+
+    def forward(self, h, M):
+        return self.address(h, M)
+
+
+
 class DynamicNeuralTuringMachineMemory(nn.Module):
     def __init__(self, n_locations, content_size, address_size, controller_input_size, controller_hidden_state_size, batch_size):
         """Instantiate the memory.
@@ -12,25 +51,21 @@ class DynamicNeuralTuringMachineMemory(nn.Module):
         address_size: size of the address part of memory locations"""
         super(DynamicNeuralTuringMachineMemory, self).__init__()
 
-        self.register_buffer("exp_mov_avg_similarity", torch.zeros(size=(n_locations, batch_size)))
         self.register_buffer("memory_contents", torch.zeros(size=(n_locations, content_size)))
-        # self.memory_contents = nn.Parameter(torch.zeros(size=(n_locations, content_size)), requires_grad=False)
         self.memory_addresses = nn.Parameter(torch.zeros(size=(n_locations, address_size)), requires_grad=True)
         self.overall_memory_size = content_size + address_size
 
-        self.W_hat_hidden = nn.Parameter(torch.zeros(size=(n_locations, controller_hidden_state_size)))
+        self.read_head = MemoryHead(
+            n_locations=n_locations,
+            location_size=self.overall_memory_size,
+            batch_size=batch_size,
+            hidden_size=controller_hidden_state_size)
 
-        # query vector MLP parameters (W_k, b_k)
-        self.W_query = nn.Parameter(torch.zeros(size=(n_locations, self.overall_memory_size)), requires_grad=True)
-        self.b_query = nn.Parameter(torch.zeros(size=(self.overall_memory_size, 1)), requires_grad=True)
-
-        # sharpening parameters (u_beta, b_beta)
-        self.u_sharpen = nn.Parameter(torch.zeros(size=(controller_hidden_state_size, 1)), requires_grad=True)
-        self.b_sharpen = nn.Parameter(torch.zeros(1), requires_grad=True)
-
-        # LRU parameters (u_gamma, b_gamma)
-        self.b_lru = nn.Parameter(torch.zeros(1))
-        self.u_lru = nn.Parameter(torch.zeros(size=(controller_hidden_state_size, 1)))
+        self.write_head =  MemoryHead(
+            n_locations=n_locations,
+            location_size=self.overall_memory_size,
+            batch_size=batch_size,
+            hidden_size=controller_hidden_state_size)
 
         # erase parameters (generate e_t)
         self.W_erase = nn.Parameter(torch.zeros(size=(content_size, controller_hidden_state_size)))
@@ -45,14 +80,14 @@ class DynamicNeuralTuringMachineMemory(nn.Module):
 
     def read(self, controller_hidden_state):
         logging.debug("Reading memory")
-        self.read_weights = self._address_memory(controller_hidden_state)
+        self.read_weights = self.read_head(controller_hidden_state, self._full_memory_view())
         # this implements the memory NO-OP at reading phase
         return self._full_memory_view()[:-1, :].T @ self.read_weights[:-1, :]
         # TODO add in tests NO-OP
 
     def update(self, controller_hidden_state, controller_input):
         logging.debug("Updating memory")
-        self.write_weights = self._address_memory(controller_hidden_state)
+        self.write_weights = self.write_head(controller_hidden_state, self._full_memory_view())
         self.erase_vector = self.W_erase @ controller_hidden_state + self.b_erase  # TODO MLP
 
         self.alpha = (self.u_input_content_alpha @ controller_input +
@@ -65,22 +100,6 @@ class DynamicNeuralTuringMachineMemory(nn.Module):
         self.memory_contents[:-1, :] = (self.memory_contents[:-1, :]
                                         - self.write_weights[:-1, :] @ self.erase_vector.T
                                         + self.write_weights[:-1, :] @ self.candidate_content_vector.T)
-
-    def _address_memory(self, controller_hidden_state):
-        self.projected_hidden_state = self.W_hat_hidden @ controller_hidden_state
-        self.query = self.W_query.T @ self.projected_hidden_state + self.b_query
-        self.sharpening_beta = F.softplus(self.u_sharpen.T @ controller_hidden_state + self.b_sharpen) + 1
-        
-        # compute similarity
-        full_memory_view = self._full_memory_view()
-        self.similarity_vector = self.sharpening_beta * F.cosine_similarity(full_memory_view[:, None, :], self.query.T[None, :, :],
-                                                                  eps=1e-7, dim=-1)
-        # lru mechanism
-        self.lru_gamma = torch.sigmoid(self.u_lru.T @ controller_hidden_state + self.b_lru)
-        self.lru_similarity_vector = F.softmax(self.similarity_vector - self.lru_gamma * self.exp_mov_avg_similarity, dim=0)
-        with torch.no_grad():
-            self.exp_mov_avg_similarity = 0.1 * self.exp_mov_avg_similarity + 0.9 * self.similarity_vector
-        return self.lru_similarity_vector
 
     def _full_memory_view(self):
         return torch.cat((self.memory_addresses, self.memory_contents), dim=1)
